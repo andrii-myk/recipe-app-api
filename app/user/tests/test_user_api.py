c8b2830dca583ad9da53d8ce92f8974a9830c8f1
drjakill <dr_jakill@outlook.com>
2020-01-07 13:58:26 +0200

    Added manage user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -17,7 +18,7 @@ def create_user(**params):
 class PublicUserApiTests(TestCase):
     """ Test the users API (public) """
 
-    def setup(self):
+    def setUp(self):
         self.client = APIClient()
 
     def test_create_valid_user_success(self):
@@ -69,8 +70,8 @@ class PublicUserApiTests(TestCase):
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    def test_token_invalid_credentionals(self):
-        """ Should check token is not created with invalid credentials 
+    def test_token_invalid_credentials(self):
+        """ Should check token is not created with invalid credentials
         are given"""
         create_user(email='test@mail.ua', password='red123456')
         payload = {'email': 'test@mail.ua', 'password': 'wrong'}
@@ -92,3 +93,51 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """ Should check authentication is required for changing profile """
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """ Test API requests that require authentication """
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@mail.ua',
+            password='red123456',
+            name='name',
+        )
+        print(self.user)
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """ Should retrieve user profile for logged in user """
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_not_allowed(self):
+        """ Should test POST is not allowed on the me url """
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """ Should check updating user profile for authenticated user """
+        payload = {'name': 'new_name', 'password': 'newpassword123'}
+
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
