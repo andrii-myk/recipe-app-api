bf450e064483086b35b8e8ce08c57536c7349f5e
andrew <andriimyk@gmail.com>
2020-03-30 22:10:06 +0300

    Added create  ingredient feature

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -10,7 +10,7 @@ from core.models import Ingredient
 from recipe.serializers import IngredientSerializer
 
 
-INGREDIENSTS_URL = reverse('recipe:ingredient-list')
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
 class PublicIngredientsApiTests(TestCase):
@@ -22,7 +22,7 @@ class PublicIngredientsApiTests(TestCase):
     def test_login_required(self):
         """ Test that login is required to access the endpoint """
 
-        res = self.client.get(INGREDIENSTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -31,26 +31,25 @@ class PrivateIngredientsApiTests(TestCase):
     """ Test the private ingredients API """
 
     def setUp(self):
-        self.client = APIClient()
         self.user = get_user_model().objects.create_user(
             'test@mail.ua',
-            'testpass'
+            'password123'
         )
+        self.client = APIClient()
         self.client.force_authenticate(self.user)
-
     def test_retrieve_ingredient_list(self):
         """ Test retrieving a list of ingredients """
         Ingredient.objects.create(user=self.user, name='Kale')
         Ingredient.objects.create(user=self.user, name='Salt')
 
-        res = self.client.get(INGREDIENSTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
         ingredients = Ingredient.objects.all().order_by('-name')
         serializer = IngredientSerializer(ingredients, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
-    def ingredients_limited_to_user(self):
+    def test_ingredients_limited_to_user(self):
         """ Test that ingredients for authenticated user are returned """
         user2 = get_user_model().objects.create_user(
             'other@mail.ua',
@@ -60,8 +59,28 @@ class PrivateIngredientsApiTests(TestCase):
 
         ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
 
-        res = self.client.get(INGREDIENSTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """ Test create a new ingredient """
+        payload = {'name': 'Cabbage'}
+        print(self.client)
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+        print(self.user)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
